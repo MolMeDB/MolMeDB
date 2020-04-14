cb3517e0f36873ff6de2c14b6006480b8ca51d85
Jakub Juracka <jakub.juracka55@gmail.com>
2020-04-14 20:00:12 +0200

    Added system config

--- application/model/Configs.php
+++ application/model/Configs.php
@@ -13,14 +13,21 @@ class Configs extends Db
     /** CONFIG ATTRIBUTES */
     const EUROPEPMC_URI = 'europepmc_uri';
     const RDKIT_URI = 'rdkit_uri';
+<<<<<<< HEAD
     const LAST_SMILES_UPDATE = 'smiles_updated_date';
+=======
+>>>>>>> a468945... Added system config
 
     /** Defines valid config attributes */
     private $valid_attributes = array
     (
         self::EUROPEPMC_URI,
+<<<<<<< HEAD
         self::RDKIT_URI,
         self::LAST_SMILES_UPDATE
+=======
+        self::RDKIT_URI
+>>>>>>> a468945... Added system config
     );
     
     /**

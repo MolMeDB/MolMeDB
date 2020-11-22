1282d94506acf5eefbbd42ffc28f0cc6f656c6ff
Jakub Juracka¨ <jakub.juracka55@gmail.com>
2020-11-22 14:59:35 +0100

    Version fix

--- version.php
+++ version.php
@@ -4,5 +4,5 @@
 define('DB_VERSION', 'molmedb~alpha_1.05');
 
 // JS/CSS version
-define("JS_VERSION", "1.4");
-define("CSS_VERSION", "1.4");
+define("JS_VERSION", "2.0");
+define("CSS_VERSION", "2.0");

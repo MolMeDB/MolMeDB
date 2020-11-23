8e9d3435a615649bf3c1704c89f9d900b5e2c210
Jakub Juracka¨ <jakub.juracka55@gmail.com>
2020-11-23 14:43:28 +0100

    Fix of DB upgrade

---
+++ db_upgrades/upgrade~alpha_1.06.php
@@ -0,0 +1,15 @@
+<?php 
+
+/**
+ * Adding columns with accuracy to the transporter interaction table
+ * Version 1.04 release
+ * 
+ * @author Jakub Juračka <jakub.juracka55@gmail.com>
+ */
+$upgrade_sql = array
+(
+   "ALTER TABLE `transporters` ADD `Km_acc` FLOAT UNSIGNED NULL DEFAULT NULL AFTER `Km`;",
+   "ALTER TABLE `transporters` ADD `EC50_acc` FLOAT UNSIGNED NULL DEFAULT NULL AFTER `EC50`;",
+   "ALTER TABLE `transporters` ADD `Ki_acc` FLOAT UNSIGNED NULL DEFAULT NULL AFTER `Ki`;",
+   "ALTER TABLE `transporters` ADD `IC50_acc` FLOAT UNSIGNED NULL DEFAULT NULL AFTER `IC50`;",
+);
--- version.php
+++ version.php
@@ -1,7 +1,7 @@
 <?php
 
 /** HOLDS INFO ABOUT DB VERSION */
-define('DB_VERSION', 'molmedb~alpha_1.05');
+define('DB_VERSION', 'molmedb~alpha_1.06');
 
 // JS/CSS version
 define("JS_VERSION", "2.0");

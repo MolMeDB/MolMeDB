8533024b63a8fd16e95407a77e20ad12ff06f83c
Jakub Juračka <jakub.juracka55@gmail.com>
2021-02-08 20:42:41 +0100

    CSS FIX

--- db_upgrades/upgrade~alpha_1.08.php
+++ db_upgrades/upgrade~alpha_1.08.php
@@ -8,6 +8,6 @@
  */
 $upgrade_sql = array
 (
-   "CREATE TABLE `stats` ( `id` INT NOT NULL AUTO_INCREMENT , `type` SMALLINT NOT NULL , `content` TEXT NULL DEFAULT NULL , `update_date` DATE NOT NULL DEFAULT CURRENT_TIMESTAMP , PRIMARY KEY (`id`)) ENGINE = InnoDB;",
+   "CREATE TABLE `stats` ( `id` INT NOT NULL AUTO_INCREMENT , `type` SMALLINT NOT NULL , `content` TEXT NULL DEFAULT NULL , `update_date` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP , PRIMARY KEY (`id`)) ENGINE = InnoDB;",
    "ALTER TABLE `stats` ADD UNIQUE (`type`);"
 );

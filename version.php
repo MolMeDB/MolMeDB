556a43c0365eb8347887dc99012f21d75ba02e72
Jakub Juračka <jakub.juracka55@gmail.com>
2022-09-08 07:31:22 +0200

    Db_upgrade + version fix

--- version.php
+++ version.php
@@ -1,8 +1,8 @@
 <?php
 
 /** HOLDS INFO ABOUT DB VERSION */
-define('DB_VERSION', 'molmedb~alpha_1.17');
+define('DB_VERSION', 'molmedb~alpha_1.18');
 
 // JS/CSS version
-define("JS_VERSION", "3.2");
-define("CSS_VERSION", "3.3");
+define("JS_VERSION", "3.3");
+define("CSS_VERSION", "3.4");
